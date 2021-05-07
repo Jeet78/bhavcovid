a9e7d9f9e2041c1b3dec79baa2dc8522a23d3470
Jeet <jeet@Jeets-MacBook-Air.local>
2021-05-07 15:05:08 +0530

    initial3

--- script.js
+++ script.js
@@ -38,5 +38,61 @@ var app = new Vue({
       "./assets/images/o2-3.jpeg",
       "./assets/images/o2-2.jpeg",
     ],
+    food: [
+      {
+        title: "Free Meal for Patients",
+        details: "Kindly call before 1 day and send all the details.",
+        morning: "11 AM - 1 PM",
+        evening: "6 PM - 7 PM",
+        org: "BHARWAD SAMAAJ, BHAVNAGAR.",
+        phn1: "9825505005",
+        phn2: "9825217804",
+      },
+      {
+        title: "Free Meal for Patients",
+        details: "Kindly call before 1 day and send all the details.",
+        morning: "Before 7 AM",
+        evening: "Before 6 PM",
+        org: "Sihor Sampraday Bhramhan Gnati.",
+        phn1: "9879557075",
+        phn2: "9925574380",
+      },
+      {
+        title: "Free Breakfast for Patients",
+        details: "Kindly send address and details of patient on whatsapp.",
+        morning: "8 AM - 10 AM",
+        evening: "Before 9PM",
+        org: "Amitbhai & Aashishbhai.",
+        phn1: "7567244189",
+        phn2: "7383412348",
+      },
+      {
+        title: "Free Coconut Water (Nariyel Pani)",
+        details:
+          "Organised by & for people of bhavnagar. (Volunteers can join)",
+        org: "Unknown",
+        phn1: "6357675555",
+      },
+      {
+        title: "Param Tiffin - JAIN (2 Times)",
+        details: "Kindly call before 1 day and send all the details.",
+        org: "Arham Yuva Seva Group",
+        phn1: "9136442493",
+      },
+    ],
+    equipments: [
+      {
+        title: "BIPAP Machine on Rent.",
+        details: "Kindly call and know more details.",
+        org: "UNKNOWN",
+        phn1: "9898317316",
+      },
+      {
+        title: "Oxygen Valve & Flow-meters",
+        details:
+          "Please visit : Jay Traders, M.G. Road (Opposite Khodiyar Icecream) OR Bhagat Trading Opposite Vaishali talkies. ",
+        org: "Jay Traders & Bhagat Trading",
+      },
+    ],
   },
 });
